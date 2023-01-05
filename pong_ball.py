ee92b993cda20746367bd87d3a4d5e853eb19a38
Cristian FLORUTA <florutacristian@gmail.com>
2023-01-05 19:52:11 +0200

    increase ball speed method have been added

--- pong_ball.py
+++ pong_ball.py
@@ -13,6 +13,7 @@ class Ball(Turtle):
         self.y_move = 10
         self.penup()
         self.goto(0, 0)
+        self.ball_speed = 0.1
 
     def move(self):
         x_coord = self.xcor() + self.x_move
@@ -28,3 +29,11 @@ class Ball(Turtle):
     def reset_position(self):
         self.goto(0, 0)
         self.bounce_x()
+
+    def increase_speed(self):
+        if self.ball_speed > 0.05:
+            self.ball_speed -= 0.01
+            return self.ball_speed
+        else:
+            self.speed = 0.04
+            return self.speed
